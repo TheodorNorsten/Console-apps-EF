2e10f1d58956f99783d7d4f1612d0e0d20896f94
Theodor Norsten <theodor.norsten@knowit.se>
2022-09-14 22:28:05 +0200

    Initial commit

---
+++ TrainBooking/TrainBooking/Persistence/TrainContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainBooking.Persistence
+{
+    class TrainContext : DbContext
+    {
+        public TrainContext() :base("name=TrainContext")
+        {
+
+        }
+    }
+}
